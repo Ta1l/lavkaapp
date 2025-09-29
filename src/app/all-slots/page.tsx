5def9390ce72b109347934462342f51488ad9839
Hank Hank <tail1z@yandex.ru>
2025-09-29 20:48:26 +0300

    fix

--- src/app/all-slots/page.tsx
+++ src/app/all-slots/page.tsx
@@ -9,6 +9,7 @@ import { getCalendarWeeks } from "@/lib/dateUtils";
 import { TimeSlot, Day } from "@/types/shifts";
 import TimeSlotComponent from "@/components/TimeSlotComponent";
 import { useSwipeable, SwipeableHandlers } from "react-swipeable";
+import Link from "next/link";
 
 interface UserSlots {
     userId: number;
@@ -173,7 +174,7 @@ export default function AllSlotsPage() {
     };
 
     return (
-        <div className="min-h-screen bg-black text-white" {...swipeHandlers}>
+        <div className="min-h-screen bg-black text-white relative pb-[70px]" {...swipeHandlers}>
             {/* Навигация по дням */}
             <div className="relative w-full pt-[43px]">
                 {/* Кнопка влево */}
@@ -255,6 +256,16 @@ export default function AllSlotsPage() {
                     </div>
                 )}
             </div>
+
+            {/* Footer с кнопкой "Назад" */}
+            <footer className="bg-[#272727] rounded-[20px] w-full h-[54px] fixed bottom-0 left-0 flex items-center justify-center">
+                <Link 
+                    href="/schedule/0" 
+                    className="absolute left-[17px] top-[9px] w-[145px] h-[37px] bg-[#000] rounded-[8px] text-white text-[14px] font-sans font-normal leading-none active:scale-95 transition-transform duration-80 flex items-center justify-center"
+                >
+                    Назад
+                </Link>
+            </footer>
         </div>
     );
 }
